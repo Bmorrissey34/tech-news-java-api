7fb275588b0f50142a8ec3c660f78a1e11262968
Bmorrissey34 <88950762+Bmorrissey34@users.noreply.github.com>
2022-05-29 13:40:40 -0400

    moved files

---
+++ src/main/java/com/techcentral/technewsjavaapi/TechNewsJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.techcentral.technewsjavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechNewsJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechNewsJavaApiApplication.class, args);
+	}
+
+}
