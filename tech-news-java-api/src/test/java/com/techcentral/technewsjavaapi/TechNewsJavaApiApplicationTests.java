d9aaa93970f55b739ec260893146b5b9a6f7e21b
Bmorrissey34 <88950762+Bmorrissey34@users.noreply.github.com>
2022-05-29 13:30:12 -0400

    initial commit

---
+++ tech-news-java-api/src/test/java/com/techcentral/technewsjavaapi/TechNewsJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techcentral.technewsjavaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechNewsJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
